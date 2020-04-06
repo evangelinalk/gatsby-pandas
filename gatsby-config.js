b29feb4ed5b6682a35154c053113575619169516
Evangelina Gamez <evangelina.gamez@lkmx.io>
2020-04-06 00:20:08 -0700

    Init commit 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@
 
 module.exports = {
   /* Your site config here */
+  pathPrefix: "/gatsby-pandas",
   siteMetadata: {
     title: `Title from siteMetadata`,
     description: `A simple description about pandas eating lots...`,
